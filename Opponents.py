1c6e2ed5776d73f305a7a9045daa0f10e0bfcb7a
nwii <Neilwei2000@gmail.com>
2021-03-29 18:51:01 -0400

    Created random process ai

---
+++ Opponents.py
@@ -0,0 +1,42 @@
+import random as rand
+import c4game
+
+
+class RP:
+    """
+    Checks 1 move ahead for a game-ending move, if none found, uses random number
+    """
+    def __init__(self, game, player):
+        self.board = game.board
+        self.player = player
+
+    def lookforend(self, move):
+        for testplayer in [self.player, (self.player % 2) +1]:
+            for i in range(0,7):
+                testboard = self.board
+                if c4game.checkvalid(testboard, i):
+                    row = c4game.getrow(testboard, i)
+                    testboard[row][i] = testplayer
+                    if c4game.checkwin(testboard, testplayer):
+                        testboard[row][i] = 0
+                        return i
+                    testboard[row][i] = 0
+        return move
+
+    def makemove(self):
+        move = rand.randint(0,6)
+        move = self.lookforend(move)
+
+        return move
+
+if __name__ == '__main__':
+    g1 = c4game.game()
+    comp1 = RP(g1, 2)
+    comp2 = RP(g1, 1)
+    while not g1.gameover:
+        if (g1.turn % 2)+1 == 1:
+            g1.makemove(comp1.makemove())
+        else:
+            g1.makemove(comp2.makemove())
+        g1.show()
+    print("Winner: {}".format(g1.winner))
