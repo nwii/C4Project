217b459b8c88793b77c6579c77a1e6ddfe0df57c
rjoeldriscoll <rdriscoll@umass.edu>
2021-04-08 19:59:09 -0400

    CNN structure & main draft

--- Opponents.py
+++ Opponents.py
@@ -40,10 +40,25 @@ class manual:
 
 class CNN:
     def __init__(self):
-        self.model = None
+        self.model = keras.Sequential(
+            [
+                layers.Conv2D(64, kernel_size=4, activation="relu", input_shape=(6,7,1)), # 64 nodes, kernel_size = filter_size = 4x4, 6x7 grid with 1 dimension
+                layers.Dense(42, activation="relu"),
+                layers.Dense(42, activation="relu"),
+                layers.Dense(42, activation="relu"),
+                layers.Dense(1, activation="softmax") # 7 ~ num. outputs (each possible column). Each will have its own probability due to the softmax activation.
+                
+                # Quote from CNN 0-9 digit classifier:
+                # "We will have 10 nodes in our output layer, one for each possible outcome (0-9). The activation is "softmax". Softmax makes the output sum up to 1 so the output can be interpreted as probabilities. The model will then make its prediction based on which option has the highest probability".
+            ])
+            
+        self.model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+        
+        # Could also compile with optimizer="rmsprop". We'll need to experiment and find the best parameters. 
+        # Categorical Crossentropy is popular for classification, so it might be worthwhile to experiment with other loss functions too. A low categorical crossentropy value means it's performing well.
 
-    def train(self):
-        pass
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train, epochs=3)
 
 class CNNagent:
     def __init__(self, model):
@@ -61,12 +76,12 @@ class CNNagent:
 
 if __name__ == '__main__':
     g1 = c4game.game()
-    comp1 = RP(g1, 2)
-    comp2 = RP(g1, 1)
+    comp1 = RP(g1)
+    comp2 = RP(g1)
     while not g1.gameover:
         if (g1.turn % 2)+1 == 1:
-            g1.makemove(comp1.makemove())
+            g1.makemove(comp1.move())
         else:
-            g1.makemove(comp2.makemove())
+            g1.makemove(comp2.move())
         g1.show()
     print("Winner: {}".format(g1.winner))
